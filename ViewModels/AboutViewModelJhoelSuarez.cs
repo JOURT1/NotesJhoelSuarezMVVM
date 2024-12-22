531ad4e6c3443f55b76178b149beb1203083fbf6
jhoel <jhoel@DESKTOP-AB4CRPA>
2024-12-22 02:20:29 -0500

    Agregar archivos de proyecto.

---
+++ ViewModels/AboutViewModelJhoelSuarez.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+internal class AboutViewModel
+{
+    public string TitleJHOELS => "Jhoel Suarez";
+    public string VersionJHOELS => AppInfo.VersionString;
+    public string MoreInfoUrlJHOELS => "https://aka.ms/maui";
+    public string MessageJHOELS => "Tengo 20 años, soy estudiante de Ingeniería de Software cursando el quinto semestre. Me gustan los deportes, en especial entrenar en el gimnasio, los carros y las motos.";
+    public ICommand ShowMoreInfoCommandJHOELS { get; }
+
+    public AboutViewModel()
+    {
+        ShowMoreInfoCommandJHOELS = new AsyncRelayCommand(ShowMoreInfo);
+    }
+
+    async Task ShowMoreInfo() =>
+        await Launcher.Default.OpenAsync(MoreInfoUrlJHOELS);
+}
---
+++ ViewModels/NoteViewModelJhoelSuarez.cs
@@ -0,0 +1,78 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Windows.Input;
+
+namespace Notes.ViewModels;
+
+internal class NoteViewModel : ObservableObject, IQueryAttributable
+{
+    private Models.Note _note;
+
+    public string TextJHOELS
+    {
+        get => _note.Text;
+        set
+        {
+            if (_note.Text != value)
+            {
+                _note.Text = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public DateTime Date => _note.Date;
+
+    public string Identifier => _note.Filename;
+
+    public ICommand SaveCommandJHOELS { get; private set; }
+    public ICommand DeleteCommandJHOELS { get; private set; }
+
+    public NoteViewModel()
+    {
+        _note = new Models.Note();
+        SaveCommandJHOELS = new AsyncRelayCommand(Save);
+        DeleteCommandJHOELS = new AsyncRelayCommand(Delete);
+    }
+
+    public NoteViewModel(Models.Note note)
+    {
+        _note = note;
+        SaveCommandJHOELS = new AsyncRelayCommand(Save);
+        DeleteCommandJHOELS = new AsyncRelayCommand(Delete);
+    }
+
+    private async Task Save()
+    {
+        _note.Date = DateTime.Now;
+        _note.Save();
+        await Shell.Current.GoToAsync($"..?saved={_note.Filename}");
+    }
+
+    private async Task Delete()
+    {
+        _note.Delete();
+        await Shell.Current.GoToAsync($"..?deleted={_note.Filename}");
+    }
+
+    void IQueryAttributable.ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        if (query.ContainsKey("load"))
+        {
+            _note = Models.Note.Load(query["load"].ToString());
+            RefreshProperties();
+        }
+    }
+
+    public void Reload()
+    {
+        _note = Models.Note.Load(_note.Filename);
+        RefreshProperties();
+    }
+
+    private void RefreshProperties()
+    {
+        OnPropertyChanged(nameof(TextJHOELS));
+        OnPropertyChanged(nameof(Date));
+    }
+}
